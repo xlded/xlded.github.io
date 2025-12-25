16f89ee5c6dcd36cb9f3cd84e6ce3cfa333398d9
hoka <83249999+xlded@users.noreply.github.com>
2025-12-25 00:46:14 -0800

    update website

--- lakida/js/init.js
+++ lakida/js/init.js
@@ -1,10 +1,14 @@
-import "./fallingEmojis.js";
-import "./sparkleHearts.js";
-import "./season.js";
-import "./timer.js";
-import "./loveLetter.js";
-import "./favorites.js";
-import "./photosCore.js";
-import "./photosLoad.js";
-import "./photosEvents.js";
-import "./blockClick.js";
+window.addEventListener("DOMContentLoaded", ()=>{
+    startTimer();
+    setSeason();
+    setupBlockClicks();
+    setupFavorites();
+    loadLoveLetter();
+    initPhotoSystem();
+    loadPhotos();
+    setupPhotoEvents();
+    startFallingEmojis();
+    startSparkleHearts();
+
+    console.log("%c All systems loaded ♥","color:pink;font-size:16px");
+});
